7e151b54bad0818ff1d2562816a96c85bde227fd
tunv1 <tunv1@newwave.vn>
2023-08-17 11:10:25 +0700

    update mvc - controller

--- mvc/core/App.php
+++ mvc/core/App.php
@@ -1,13 +1,39 @@
 <?php 
 class App{
 
-    protected $controller;
-    protected $action;
-    protected $params;
+    protected $controller="Home";
+    protected $action="SayHi";
+    protected $params=[];
 
     function __construct()
     {
-     echo $_GET['url'];   
+        $arr = $this->urlProcess();
+     
+        if(file_exists("./mvc/controllers/".$arr[0].".php")){
+            $this->controller = $arr[0];
+            unset($arr[0]);
+        }
+        require_once "./mvc/controllers/".$this->controller.".php";
+
+        // Xu li action
+        if(isset($arr[1])){
+            if(method_exists($this->controller, $arr[1])){
+                $this->action = $arr[1];
+                
+            }
+            unset($arr[1]);
+        }
+
+        // Xu li Prams
+        $this->params = $arr ? array_values($arr) : [];
+
+        call_user_func_array([$this->controller, $this->action], $this->params);
+    }
+
+    function urlProcess(){
+        if(isset($_GET['url'])){
+            return explode('/', filter_var(trim($_GET['url'], '/'))); 
+        }
     }
 }
 ?>
